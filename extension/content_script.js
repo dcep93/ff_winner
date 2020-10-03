acd2e8f7cfea4b10f67d877eb085fab97fe127dc
dcep93 <dan@quizlet.com>
2020-10-02 17:12:07 -0700

    ts

--- extension/content_script.js
+++ extension/content_script.js
@@ -20,5 +20,6 @@ function fileToPromise(fileName) {
   const url = chrome.runtime.getURL(fileName);
   return fetch(url)
     .then((response) => response.text())
+    .then((code) => window.ts.transpile(code))
     .then(eval);
 }
