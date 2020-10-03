9062cb681f65199cf8ea78c41e9fd34e624a4f46
dcep93 <dan@quizlet.com>
2020-10-02 19:49:58 -0700

    log

--- extension/render_distribution.ts
+++ extension/render_distribution.ts
@@ -1,4 +1,6 @@
 function renderDistribution(distribution: dataToDistributionType) {
   console.log(arguments.callee.name);
-  console.log(distribution);
+  console.log(
+    distribution.map((d) => d.map((i) => i.p * i.v).reduce((a, b) => a + b, 0))
+  );
 }
