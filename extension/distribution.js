397271412e4015e6e4f4d0e9895fa5351bf8f7c1
dcep93 <dan@quizlet.com>
2020-10-02 20:48:47 -0700

    fix

--- extension/distribution.js
+++ extension/distribution.js
@@ -1,4 +1,18 @@
+const key = "distribution_v0.1";
+
+const timeout = setTimeout(
+  () => render(JSON.parse(localStorage.getItem(key))),
+  100
+);
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  alert(message);
+  clearTimeout(timeout);
+  localStorage.setItem(key, JSON.stringify(message));
+  render(message);
   sendResponse(true);
 });
+
+function render(distribution) {
+  console.log("rendering");
+  alert(JSON.stringify(distribution));
+}
