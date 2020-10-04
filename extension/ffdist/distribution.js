f127b46201713f71fbc2464d2b94129ca27417c9
dcep93 <dan@quizlet.com>
2020-10-03 19:13:02 -0700

    distribution.js

--- extension/ffdist/distribution.js
+++ extension/ffdist/distribution.js
@@ -15,35 +15,37 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   sendResponse(true);
 });
 
+//
+
 function render(data) {
-  renderDistribution(data.ds);
+  renderDistribution(data.dists);
 }
 
-function renderDistribution(distribution) {
-  const p1 = cumProb(distribution[0]);
-  const p2 = cumProb(distribution[1]);
+function renderDistribution(dists) {
+  const t1 = cumProb(dists[0]);
+  const t2 = cumProb(dists[1]);
 
-  const threshold = chooseInflectionPoint(p1, p2);
+  const threshold = chooseInflectionPoint(t1, t2);
 
-  const diff = cumProb(distribution[2]);
+  const diff = cumProb(dists[2]);
 
-  plot("#teams", { green: p1, purple: p2 }, threshold);
+  plot("#teams", { green: t1, purple: t2 }, threshold);
   plot("#diff", { green: diff }, null);
 }
 
-function cumProb(d) {
-  const low = d[0].v;
-  const high = d[d.length - 1].v;
+function cumProb(dist) {
+  const low = dist[0].v;
+  const high = dist[dist.length - 1].v;
   const size = (high - low) / (num_points - 1);
-  const map = {};
-  d.forEach((i) => {
+  const scoreMap = {};
+  dist.forEach((i) => {
     var score = Math.floor((i.v - low) / size) * size + low;
-    map[score] = i.p + (map[score] || 0);
+    scoreMap[score] = i.p + (scoreMap[score] || 0);
   });
   var prob = 0;
   return (
-    Object.keys(map)
-      .map((i) => [parseFloat(i), map[i]])
+    Object.keys(scoreMap)
+      .map((i) => [parseFloat(i), scoreMap[i]])
       .sort((a, b) => a[0] - b[0])
       .map((i) => {
         prob += i[1];
@@ -55,27 +57,29 @@ function cumProb(d) {
 }
 
 // this method is sus
-function chooseInflectionPoint(p1, p2) {
-  p1 = p1.slice();
-  p2 = p2.slice();
+function chooseInflectionPoint(t1, t2) {
+  t1 = t1.slice();
+  t2 = t2.slice();
   var probs = [];
-  var x1 = p1.shift();
-  var x2 = p2.shift();
-  while (x1[1] && x2[1]) {
-    if (x1[0] < x2[0]) {
-      probs.push([x1[0], x1[1], x2[1]]);
-      x1 = p1.shift();
+  var p1 = t1.shift();
+  var p2 = t2.shift();
+  while (p1[1] && p2[1]) {
+    if (p1[0] < p2[0]) {
+      probs.push([p1[0], p1[1], p2[1]]);
+      p1 = t1.shift();
     } else {
-      probs.push([x2[0], x1[1], x2[1]]);
-      x2 = p2.shift();
+      probs.push([p2[0], p1[1], p2[1]]);
+      p2 = t2.shift();
     }
   }
-  var i1 = probs
-    .map((i) => [i[0], i[2] * (1 - i[1])])
+  var i1 = teamIdeal(probs, 1);
+  var i2 = teamIdeal(probs, 2);
+  return (i1[0] + i2[0]) / 2;
+}
+
+function teamIdeal(probs, index) {
+  return probs
+    .map((i) => [i[0], i[3 - index] * (1 - i[index])])
     .filter((i) => i[1])
     .reduce((a, b) => (a[1] > b[1] ? a : b), [0, 0]);
-  var i2 = probs
-    .map((i) => [i[0], i[1] * (1 - i[2])])
-    .reduce((a, b) => (a[1] > b[1] ? a : b), [0, 0]);
-  return (i1[0] + i2[0]) / 2;
 }
